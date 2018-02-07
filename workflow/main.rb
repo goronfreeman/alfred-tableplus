06bfed1d3f8fe379ab1cd10533c26a66d35d60ce
Hunter Braun <hunter.braun@gmail.com>
2018-02-07 00:01:17 -0600

    Add DB icons

--- workflow/main.rb
+++ workflow/main.rb
@@ -3,49 +3,57 @@ require_relative 'lib/plist/plist'
 
 module AlfredTablePlus
   class Browse
-    SUPPORTED_DBS = %w(mysql postgresql sqlite).freeze
+    SUPPORTED_DBS = %w[mysql postgresql sqlite].freeze
 
-    attr_reader :workflow, :path, :connections
+    attr_reader :workflow, :connections
 
     def initialize
       @workflow = Alfred3::Workflow.new
-      @path = "#{ENV['HOME']}/Library/Application Support/com.tinyapp.TablePlus/Data/Connections.plist"
-      @connections = Plist.parse_xml(path)
+      plist = "#{ENV['HOME']}/Library/Application Support/com.tinyapp.TablePlus/Data/Connections.plist"
+      @connections = Plist.parse_xml(plist)
     end
 
     def open
-      output_json
+      connections.empty? ? empty_json : output_json
       print workflow.output
     end
 
-    def supported_adapter?(adapter)
-      SUPPORTED_DBS.include?(adapter)
+    def supported_driver?(driver)
+      SUPPORTED_DBS.include?(driver)
     end
 
-    def build_connection_string(adapter, connection)
-      return connection['DatabasePath'] if adapter == 'sqlite'
+    def build_connection_string(driver, connection)
+      return connection['DatabasePath'] if driver == 'sqlite'
 
       host = connection['DatabaseHost']
       db_name = connection['DatabaseName']
       user = connection['DatabaseUser']
 
-      "#{adapter}://#{user}@#{host}/#{db_name}"
+      "#{driver}://#{user}@#{host}/#{db_name}"
     end
 
     def output_json
       connections.each do |connection|
-        adapter = connection['Driver'].downcase
-        next unless supported_adapter?(adapter)
-        connection_string = build_connection_string(adapter, connection)
+        driver = connection['Driver'].downcase
+        next unless supported_driver?(driver)
+        connection_string = build_connection_string(driver, connection)
 
         workflow.result
                 .uid(connection['ID'])
                 .title(connection['ConnectionName'])
                 .subtitle(connection['DatabaseName'])
                 .arg(connection_string)
+                .icon("img/#{driver}.png")
                 .text('copy', connection_string)
                 .autocomplete(connection['ConnectionName'])
       end
+    end
+
+    def empty_json
+      workflow.result
+              .title('No database connections available!')
+              .subtitle('Open TablePlus to add a database connection')
+              .arg('-a TablePlus')
     end
   end
 end
